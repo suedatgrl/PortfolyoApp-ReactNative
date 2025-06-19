84130faafd52ddd8bb02e2d168af2b20c82b9f88
Sueda Tugrul <tugrulsueda1@gmail.com>
2025-06-19 21:47:14 +0300

    LATEST

--- utils/api.js
+++ utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
-  baseURL: "http://192.168.173.201:4000", 
+  baseURL: "https://portfolyoapp-nodejs-backend.onrender.com", 
   timeout: 5000,
 });
 
